af94b18b13d208a27362389280efe435ea4441f4
Pavel Popov <pcveykos@gmail.com>
2022-12-16 16:48:57 +0100

    Add some work on inserting tracks

--- src/app.js
+++ src/app.js
@@ -6,20 +6,31 @@ const app = express();
 app.use(express.json())
 
 
-app.use('/api/tracks/:id', async (req,res) => {
+app.post('/api/tracks', async (req,res) => {
+  const {title,youtube_url,id_album} = req.body
+  connection.query(
+    'INSERT INTO track (title,youtube_url,id_album) VALUES (?,?,?)', [title,youtube_url,id_album],
+    (err,_) => {
+      if (err) return console.log(err)
+      return connection.query('')
+    }
+    )
+})
+
+app.get('/api/tracks/:id', async (req,res) => {
   const {id} = req.params
   connection.query(
     'SELECT * FROM track WHERE id = ?', [id],
     (err,results) => {
-      if (err) console.log(err)
+      if (err) return console.log(err)
       return res.json(...results)
     }
   )
 })
-app.use('/api/tracks', async (req, res) => {
+app.get('/api/tracks', async (req, res) => {
   connection.query(
     'SELECT * FROM track', (err,results) => {
-      if (err) console.log(err);
+      if (err) return console.log(err);
       return res.json(results)
     }
   )
