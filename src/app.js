2452e7a6ee2a8dc6d03c6827758fee7745510255
Pavel Popov <pcveykos@gmail.com>
2022-12-16 16:09:13 +0100

    Add route /api/tracks/:id to return a track by id

--- src/app.js
+++ src/app.js
@@ -3,6 +3,17 @@ const express = require('express');
 const connection = require('./db')
 
 const app = express();
+app.use(express.json())
+app.use('/api/tracks/:id', async (req,res) => {
+  const {id} = req.params
+  connection.query(
+    'SELECT * FROM track WHERE id = ?', [id],
+    (err,results) => {
+      if (err) console.log(err)
+      res.send(results)
+    }
+  )
+})
 app.use('/api/tracks', async (req, res) => {
   connection.query(
     'SELECT * FROM track', (err,results) => {
@@ -11,6 +22,7 @@ app.use('/api/tracks', async (req, res) => {
     }
   )
 })
+
 
 
 app.use('/api', async (req, res) => {
