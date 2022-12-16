dda4541de089d62a7effe240f58b688d937395ae
Pavel Popov <pcveykos@gmail.com>
2022-12-16 15:57:43 +0100

    Add /api/tracks route to return all tracks

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 require('dotenv').config();
 const express = require('express');
-
+const connection = require('./db')
 
 const app = express();
+app.use('/api/tracks', async (req, res) => {
+  connection.query(
+    'SELECT * FROM track', (err,results) => {
+      if (err) console.log(err);
+      res.send(results)
+    }
+  )
+})
+
+
+app.use('/api', async (req, res) => {
+  res.send('Hello world')
+})
+
 
 
 
