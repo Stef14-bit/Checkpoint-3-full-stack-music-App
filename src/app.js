4a2f5e1140ed65ddd1c4f6197cd0a9d52cabc9b9
stefan <stefan@stefan.com>
2023-01-02 16:19:40 +0100

    first route working

--- src/app.js
+++ src/app.js
@@ -1,7 +1,34 @@
 require('dotenv').config();
 const express = require('express');
+const connection = require('../db');
+
+connection
+  .promise()
+  .query('use music')
+  .catch((err) => {
+    console.error(err);
+  });
 
 const app = express();
+
+app.get('/api/tracks', (req, res) => {
+  connection
+    .promise()
+    .query('SELECT * FROM track')
+    .then(([response]) => {
+      if (response.length) {
+        res.send(response);
+      }
+    });
+});
+
+app.get('/api/tracks/:id', (req, res) => {
+  res.json([{ hello: 'hello world' }]);
+});
+
+app.get('/', (req, res) => {
+  res.json([{ hello: 'hello world' }]);
+});
 
 // Please keep this module.exports app, we need it for the tests !
 module.exports = app;
